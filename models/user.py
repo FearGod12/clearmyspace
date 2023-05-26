34bca742c7f19276fb4a18dbb5dccb47993821d7
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-05-26 12:03:49 +0100

    Set User.email as unique

--- models/user.py
+++ models/user.py
@@ -14,7 +14,7 @@ class User(BaseModel, Base):
     username = Column(String(255), nullable=False)
     firstname = Column(String(255), nullable=False)
     lastname = Column(String(255), nullable=False)
-    email = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
     password = Column(String(255), nullable=False)
     address = Column(String(255), nullable=True)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=True)
