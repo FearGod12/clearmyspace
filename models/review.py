a22bddd52436a63fc27a22a5975a5de1a28b5f42
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-05-26 17:04:10 +0100

    Update reviews relationship to users

--- models/review.py
+++ models/review.py
@@ -3,6 +3,7 @@
 
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class Review(BaseModel, Base):
@@ -12,3 +13,9 @@ class Review(BaseModel, Base):
     author_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
+
+    author = relationship('User', foreign_keys=[author_id],
+                          backref=backref('authored_reviews',
+                                          cascade='all, delete'))
+    user = relationship('User', foreign_keys=[user_id],
+                        backref=backref('reviews', cascade='all, delete'))
