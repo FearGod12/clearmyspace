9c2e365732da6f5a855af7bd6bb44f0ee1a2fdb3
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-05-26 14:10:43 +0100

    Create Chats API endpoints

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,3 +8,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.status import *  # noqa
 from api.v1.views.items import *  # noqa
 from api.v1.views.users import *  # noqa
+from api.v1.views.chats import *  # noqa
